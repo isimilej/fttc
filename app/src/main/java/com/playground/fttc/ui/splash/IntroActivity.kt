0ae2aa1ac9ce891c7705afa4548737daaa94a41e
kwangjin <isimilej@gmail.com>
2024-08-01 11:06:28 +0900

    [K-2] 스타일 수정
    - Color, Font 추가

--- app/src/main/java/com/playground/fttc/ui/splash/IntroActivity.kt
+++ app/src/main/java/com/playground/fttc/ui/splash/IntroActivity.kt
@@ -8,13 +8,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.lifecycleScope
+import androidx.compose.ui.unit.sp
 import com.playground.fttc.ui.BaseActivity
 import com.playground.fttc.ui.theme.FTTCTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class SplashActivity : BaseActivity() {
+class IntroActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,16 +27,17 @@ class SplashActivity : BaseActivity() {
             }
         }
 
-        lifecycleScope.launch {
-            delay(1_000)
-            goLogin()
-        }
+//        lifecycleScope.launch {
+//            delay(1_000)
+//            goLogin()
+//        }
     }
 
     @Composable
     private fun initLayout() {
         Text(
-            text = "Splash..."
+            text = "Splash...", style = MaterialTheme.typography.bodyLarge,
+            fontSize = 10.sp
         )
     }
 }
--- app/src/main/java/com/playground/fttc/ui/theme/Type.kt
+++ app/src/main/java/com/playground/fttc/ui/theme/Type.kt
@@ -2,9 +2,40 @@ package com.playground.fttc.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.playground.fttc.R
+
+val Pretendards = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+)
+
+val T32 = TextStyle(
+    fontFamily = Pretendards,
+    fontWeight = FontWeight.Normal,
+    fontSize = 32.sp,
+    lineHeight = 42.sp,
+    letterSpacing = 0.5.sp
+)
+
+val T32SB = T32.copy(fontWeight = FontWeight.SemiBold)
+val T32B = T32.copy(fontWeight = FontWeight.Bold)
+
+val T28 = TextStyle(
+    fontFamily = Pretendards,
+    fontWeight = FontWeight.Normal,
+    fontSize = 28.sp,
+    lineHeight = 40.sp,
+    letterSpacing = 0.5.sp
+)
+val T28SB = T28.copy(fontWeight = FontWeight.SemiBold)
+val T28B = T28.copy(fontWeight = FontWeight.Bold)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
