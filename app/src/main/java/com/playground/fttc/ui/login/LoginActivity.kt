858c4d5e9010009b765365ba696cc6a3a0f63cd5
kwangjin <isimilej@gmail.com>
2024-08-02 17:56:13 +0900

    [K-5] 로그인 화면 구성 중
    - TextField, Button 추가

--- app/src/main/java/com/playground/fttc/ui/login/LoginActivity.kt
+++ app/src/main/java/com/playground/fttc/ui/login/LoginActivity.kt
@@ -5,14 +5,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.playground.fttc.R
 import com.playground.fttc.ui.home.HomeActivity
 import com.playground.fttc.ui.theme.FTTCTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class LoginActivity : ComponentActivity() {
 
@@ -31,17 +43,6 @@ class LoginActivity : ComponentActivity() {
         }
     }
 
-    @Composable
-    private fun initLayout() {
-        Text(
-            text = "Login",
-            modifier = Modifier
-                .clickable {
-                    goHome()
-                }
-        )
-    }
-
     private fun goHome() {
         startActivity(Intent(this, HomeActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
@@ -50,5 +51,44 @@ class LoginActivity : ComponentActivity() {
             addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
         })
     }
+}
 
+@Composable
+private fun initLayout() {
+
+    Column {
+        TextField(value = "id,", onValueChange = {
+
+        }, singleLine = true)
+        TextField(value = "password,", onValueChange = {
+
+        })
+        Text(text = stringResource(id = R.string.login))
+        Button(
+            onClick = {},
+            modifier = Modifier.wrapContentSize(),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.Black,
+                containerColor = Color.Red,
+            ),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Text(text = "Login")
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun preview() {
+    FTTCTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            initLayout()
+        }
+    }
 }
