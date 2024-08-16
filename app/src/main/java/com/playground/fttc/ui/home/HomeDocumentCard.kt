c3b0a31f6a407c0dbca6e989988d9d99c73ef7e7
kwangjin <isimilej@gmail.com>
2024-08-16 17:53:09 +0900

    [K-11] 여신 기간 연장 카드 수정
    - 홈 화면 '여신 기간 연장 카드' 디자인 적용

--- app/src/main/java/com/playground/fttc/ui/home/HomeDocumentCard.kt
+++ app/src/main/java/com/playground/fttc/ui/home/HomeDocumentCard.kt
@@ -1,16 +1,25 @@
 package com.playground.fttc.ui.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.playground.fttc.R
 import com.playground.fttc.ui.theme.FttcStyle
 
@@ -20,17 +29,48 @@ fun HomeDocumentCard() {
         modifier = Modifier.width(400.dp),
         shape = RoundedCornerShape(32.dp),
         colors = CardDefaults.cardColors(
-            containerColor = FttcStyle.color.Blue800
+            containerColor = FttcStyle.color.White
         ),
         elevation = CardDefaults.outlinedCardElevation(
             defaultElevation = 16.dp
         )
     ) {
-        Text(text = "333333")
-        Image(
-            painter = painterResource(id = R.drawable.ic_home_document),
-            null
-        )
+        Row(
+            modifier = Modifier
+                .padding(start = 40.dp, top = 32.dp, end = 32.dp, bottom = 32.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Text(
+                    text = "여신 기간연장\n서식 작성 가능 고객",
+                    style = FttcStyle.typo.T2SemiBold
+                )
+                Row(
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(
+                        text = "5",
+                        style = FttcStyle.typo.Base.copy(
+                            color = FttcStyle.color.Blue600,
+                            fontSize = 40.sp,
+                            lineHeight = 52.sp,
+                            fontWeight = FontWeight.Bold,
+                            textDecoration = TextDecoration.Underline
+                        )
+                    )
+                    Text(
+                        text = "건",
+                        modifier = Modifier.padding(start = 6.dp, bottom = 4.dp),
+                        style = FttcStyle.typo.B1.copy(color = FttcStyle.color.Grey600)
+                    )
+                }
+            }
+            Image(
+                painter = painterResource(id = R.drawable.ic_home_document),
+                contentDescription = null,
+            )
+        }
     }
 }
 
