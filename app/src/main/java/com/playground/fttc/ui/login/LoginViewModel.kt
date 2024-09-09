67df758d21193faed1651744cf12215212fff03f
kwangjin <isimilej@gmail.com>
2024-09-09 17:55:06 +0900

    [K-27] ViewModel Factory 적용
    - Repository DI Factory

--- app/src/main/java/com/playground/fttc/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/playground/fttc/ui/login/LoginViewModel.kt
@@ -2,15 +2,17 @@ package com.playground.fttc.ui.login
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.playground.fttc.data.user.FakeUserRepository
 import com.playground.fttc.domain.user.LoginResult
 import com.playground.fttc.domain.user.UserRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(
+    private val repository: UserRepository,
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow<LoginUiState>(LoginUiState.Ready)
     var uiState = _uiState.asStateFlow()
@@ -21,8 +23,6 @@ class LoginViewModel : ViewModel() {
 
     fun login(userId: String, password: String) {
         Log.d("LoginViewModel", "Login($userId, $password)")
-
-        val repository: UserRepository = FakeUserRepository()
 
         viewModelScope.launch {
             if (userId.isEmpty()) {
@@ -48,4 +48,19 @@ class LoginViewModel : ViewModel() {
             }
         }
     }
+
+    /**
+     * Factory for LoginViewModel that takes PostsRepository as a dependency
+     */
+    companion object {
+        fun provideFactory(
+            repository: UserRepository,
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return LoginViewModel(repository) as T
+            }
+        }
+    }
+
 }
